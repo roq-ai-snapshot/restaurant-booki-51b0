810e5ed448c449b0d5ee388e7b5375122f5fae2f
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-11-01 11:01:02 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,27 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Restaurant Owner'],
+  customerRoles: ['Customer'],
+  tenantRoles: ['Restaurant Owner', 'Restaurant Manager'],
+  tenantName: 'Restaurant',
+  applicationName: 'Restaurant booking engine',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: ['Read restaurant information', 'Read menus', 'Book a table', 'Edit personal info'],
+  ownerAbilities: [
+    'Manage restaurant information',
+    'Manage restaurant menus',
+    'Manage restaurant bookings',
+    'Manage restaurant employees',
+  ],
+  getQuoteUrl: 'https://roq-wizzard-git-qa03-roqtech.vercel.app/proposal/90e2328a-17e4-4068-a076-e33266e75333',
+};
---
+++ src/interfaces/reservation/index.ts
@@ -0,0 +1,24 @@
+import { TableInterface } from 'interfaces/table';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface ReservationInterface {
+  id?: string;
+  date: any;
+  time: any;
+  number_of_people: number;
+  table_id: string;
+  user_id: string;
+  created_at?: any;
+  updated_at?: any;
+
+  table?: TableInterface;
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface ReservationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  table_id?: string;
+  user_id?: string;
+}
